f9c7bbdc638ce1695d4d8ba90f0b3632ae9aac74
patrick-potocny <pato.potocny85@gmail.com>
2022-10-11 21:53:30 +0200

    Finished set-up started designing

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "ignorePatterns": ['webpack.config.js', '.eslintrc.js', 'dist/bundle.js'],
+    "rules": {
+    }
+}
